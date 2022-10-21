b9729d7ea353a873fa9db83b4a2dfa5440610e75
marco antonio monteiro capote <marcocapote15@gmail.com>
2022-10-20 21:44:25 -0300

    caça-palavras

---
+++ js/script.js
@@ -0,0 +1,24 @@
+matriz = new Array;
+matriz = [
+    ["","","","","L","C","","","","U","V","A"],
+    ["","","M","E","L","A","N","C","I","A","",""],
+    ["","","","","","R","R","","","","",""],
+    ["","","B","A","N","A","N","A","","","",""],
+    ["P","","","","","M","M","A","N","G","A",""],
+    ["Ê","","","","","B","","","","J","",""],
+    ["S","M","E","L","Ã","O","","","","","A",""],
+    ["S","","","","","L","","","","","",""],
+    ["E","","","","","A","","","M","","",""],
+    ["G","","","","","","T","O","M","A","T","E"],
+    ["O","","","","","","","","","","Ç",""],
+    ["","","","","","","","","","","","Ã"]
+];
+function letra(){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+function teste(){
+    document.write(Date());
+}
+document.write("sdfasd");
